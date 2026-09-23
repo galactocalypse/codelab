98a9e615de68e174c589356770f6bef3401fb744
Adarsh Yagnik <galactocalypse@gmail.com>
2026-09-23 17:30:41 +0530

    Update module defs

--- codelab-movies/src/main/java/com/codelab/movies/autoconfigure/MoviesModuleProvider.java
+++ codelab-movies/src/main/java/com/codelab/movies/autoconfigure/MoviesModuleProvider.java
@@ -3,15 +3,16 @@ package com.codelab.movies.autoconfigure;
 import com.codelab.common.spring.persistence.CodelabModule;
 import com.codelab.common.spring.persistence.CodelabModuleProvider;
 
+import java.util.Map;
+
 public class MoviesModuleProvider implements CodelabModuleProvider {
-    @Override
-    public CodelabModule provide() {
-        return new CodelabModule(
-                "movies",
-                "moviesPU",
-                "com.codelab.movies.entity",
-                "com.codelab.movies.repository",
-                "codelab"
-        );
-    }
+  @Override
+  public CodelabModule provide() {
+    return new CodelabModule(
+        "movies",
+        "com.codelab.movies",
+        "moviesPU",
+        "movies",
+        Map.of());
+  }
 }
